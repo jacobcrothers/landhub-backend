b688009fcd4ba1d197b46addada2893830b55fd2
jacobcrothers <jacobcrothers425@gmail.com>
2021-06-18 20:27:33 -0500

    update model

--- LandHubWebService/Command/CreateMailhouseCommand.cs
+++ LandHubWebService/Command/CreateMailhouseCommand.cs
@@ -8,7 +8,7 @@ namespace Commands
 {
     public class CreateMailhouseCommand : IRequest
     {
-        public string MailhouseName { get; set; }
+        public string Name { get; set; }
        
         public string Address { get; set; }
         public string Email { get; set; }
