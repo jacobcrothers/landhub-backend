d01344f423a1daf71f2633a471c22b9202814610
jacobcrothers <jacobcrothers425@gmail.com>
2021-06-19 23:14:14 -0500

    Updated model to include api key. Renamed fields.

--- LandHubWebService/Command/CreateMailhouseCommand.cs
+++ LandHubWebService/Command/CreateMailhouseCommand.cs
@@ -10,12 +10,12 @@ namespace Commands
     {
         public string Name { get; set; }
        
-        public string Address { get; set; }
-        public string Email { get; set; }
+        public string APIKey { get; set; }
+        public string ContactEmail { get; set; }
         public string PhoneNumber { get; set; }
         public string OrganizationId { get; set; }
         public DateTime CreatedOn { get; set; }
-        public string CreatedBy { get; set; }
+        public string ContactName { get; set; }
 
         [JsonIgnore]
         public string MailhouseId { get; set; }
--- LandHubWebService/Domain/DBModels/Mailhouse.cs
+++ LandHubWebService/Domain/DBModels/Mailhouse.cs
@@ -9,10 +9,10 @@ namespace Domains.DBModels
     {
         public string Name { get; set; }
         public string OrganizationId { get; set; }
-        public string Address { get; set; }
-        public string Email { get; set; }
+        public string APIKey { get; set; }
+        public string ContactEmail { get; set; }
         public string PhoneNumber { get; set; }
         public DateTime CreatedOn { get; set; }
-        public string CreatedBy { get; set; }
+        public string ContactName { get; set; }
     }
 }
