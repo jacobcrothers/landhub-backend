2f9670949ad24364ca495bc4c58ade542248c0cf
jacobcrothers <jacobcrothers425@gmail.com>
2021-06-23 14:48:32 +0600

    Mail house update

--- LandHubWebService/Domain/DBModels/Mailhouse.cs
+++ LandHubWebService/Domain/DBModels/Mailhouse.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Domains.DBModels
 {
@@ -9,6 +6,8 @@ namespace Domains.DBModels
     {
         public string Name { get; set; }
         public string OrganizationId { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
         public string APIKey { get; set; }
         public string ContactEmail { get; set; }
         public string PhoneNumber { get; set; }
