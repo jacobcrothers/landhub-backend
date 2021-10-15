e7bbd41d5334c15a3b622d2af6fcd75740a5732d
jacobcrothers <jacobcrothers425@gmail.com>
2021-10-15 23:00:46 +0530

    Campaign API added

---
+++ LandHubWebService/CommandHandler/UpdateCampaignCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Commands;
+using Domains.DBModels;
+using MediatR;
+using Services.Repository;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CommandHandlers
+{
+    public class UpdateCampaignCommandHandler : AsyncRequestHandler<UpdateCampaignCommand>
+    {
+        private readonly IMapper _mapper;
+        private IBaseRepository<Campaign> _baseRepositoryCampaign;
+
+        public UpdateCampaignCommandHandler(IMapper mapper
+            , IBaseRepository<Campaign> _baseRepositoryCampaign
+        )
+        {
+            _mapper = mapper;
+            this._baseRepositoryCampaign = _baseRepositoryCampaign;
+        }
+
+        protected override async Task Handle(UpdateCampaignCommand request, CancellationToken cancellationToken)
+        {
+            var campaign = _mapper.Map<UpdateCampaignCommand, Campaign>(request);
+            await _baseRepositoryCampaign.UpdateAsync(campaign);
+        }
+
+    }
+}
