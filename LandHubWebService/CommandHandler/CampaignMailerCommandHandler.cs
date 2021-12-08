2dd0debb9530465e7fe3f73fce59516a089d2330
jacobcrothers <jacobcrothers425@gmail.com>
2021-12-08 08:58:07 +0530

    poc

---
+++ LandHubWebService/CommandHandler/CampaignMailerCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Commands;
+using Domains.DBModels;
+using MediatR;
+using PropertyHatchCoreService.Services;
+using Services.Repository;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CommandHandlers
+{
+    public class CampaignMailerCommandHandler : IRequestHandler<CampaignMailerCommand, string>
+    {
+        private IBaseRepository<Campaign> _campaignRepository;
+        private PostCardManiaService _postCardManiaService;
+        public CampaignMailerCommandHandler(IBaseRepository<Campaign> campaignRepository, PostCardManiaService postCardManiaService)
+        {
+            this._campaignRepository = campaignRepository;
+            this._postCardManiaService = postCardManiaService;
+        }
+
+        public async Task<string> Handle(CampaignMailerCommand request, CancellationToken cancellationToken)
+        {
+            var orders = (await _postCardManiaService.GetAllOrdersAsync()).Count;
+            return "Campaigns: " + this._campaignRepository.GetTotalCount(x => true) + ";Orders: " + orders;
+        }
+    }
+}
---
+++ LandHubWebService/LandHubWebService/Controllers/PcmWrapperController.cs
@@ -0,0 +1,35 @@
+﻿using Commands;
+using Commands.Query;
+
+using Domains.DBModels;
+
+using MediatR;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using PropertyHatchWebApi.ApplicationContext;
+using System;
+using System.Threading.Tasks;
+
+namespace PropertyHatchWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PcmWrapperController : BaseController
+    {
+        private readonly IMediator _mediator;
+        public PcmWrapperController(IMediator mediator)
+        {
+            this._mediator = mediator;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<string> Campaign([FromBody] CampaignMailerCommand command = null)
+        {
+            if (command == null)
+                command = new CampaignMailerCommand();
+            return await _mediator.Send(command);
+        }
+    }
+}
