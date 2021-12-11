7102a9dd1073a203eeb50d974268e88a5e01b8fe
jacobcrothers <jacobcrothers425@gmail.com>
2021-12-12 03:51:57 +0900

    Search by APN, Made Filter

--- LandHubWebService/CommandHandler/QueryHandlers/GetAllPropertiesQueryHandler.cs
+++ LandHubWebService/CommandHandler/QueryHandlers/GetAllPropertiesQueryHandler.cs
@@ -13,15 +13,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace CommandHandlers.QueryHandlers
 {
     public class GetAllPropertiesQueryHandler : IRequestHandler<GetAllPropertiesQuery, List<PropertyForList>>
     {
-
         private readonly IBaseRepository<Properties> _baseRepositoryProperties;
         private readonly IMapper _mapper;
-
 
         public GetAllPropertiesQueryHandler(IMapper mapper
              , IBaseRepository<Properties> baseRepositoryProperties
@@ -35,25 +34,52 @@ namespace CommandHandlers.QueryHandlers
         {
             var propertyForList = new List<PropertyForList>();
             var properties = await _baseRepositoryProperties.GetAllWithPagingAsync(x => x.OrgId == request.OrgId, request.PageNumber, request.PageSize);
-            if (request.SearchKey == null || request.SearchKey == "")
+            var allowed = new List<bool>();
+            foreach (var property in properties.ToList())
+            {
+                allowed.Add(true);
+            }
+
+            if (request.SearchKey != null && request.SearchKey.Length > 0)
+            {
+                int j = 0;
+                foreach (var property in properties.ToList())
+                {
+                    if (property.PropertyAddress.Contains(request.SearchKey) == false && property.OwnerName.Contains(request.SearchKey) == false && property.APN.Contains(request.SearchKey) == false)
+                        allowed[j] = false;
+                    j++;
+                }
+            }
+
+            int w = 0;
+            if(request.FilterObj != null)
             {
                 foreach (var property in properties.ToList())
+                {
+                    if (request.FilterObj[0] != null && request.FilterObj[0].Length > 0 && property.PZip != request.FilterObj[0])
+                        allowed[w] = false;
+                    if (request.FilterObj[1] != null && request.FilterObj[1].Length > 0 && property.OwnerName != request.FilterObj[1])
+                        allowed[w] = false;
+                    if (request.FilterObj[2] != null && request.FilterObj[2].Length > 0 && property.PCity != request.FilterObj[2])
+                        allowed[w] = false;
+                    if (request.FilterObj[3] != null && request.FilterObj[3].Length > 0 && property.CountyName != request.FilterObj[3])
+                        allowed[w] = false;
+                    if (request.FilterObj[4] != null && request.FilterObj[4].Length > 0 && property.PropertyStatus != request.FilterObj[4])
+                        allowed[w] = false;
+                    w++;
+                }
+            }
+
+            w = 0;
+            foreach (var property in properties.ToList())
+            {
+                if (allowed[w])
                 {
                     var propertyForUi = _mapper.Map<Properties, PropertyForList>(property);
                     propertyForUi.CampaignStatus = propertyForUi.CampaignStatus ?? "Not Assigned";
                     propertyForList.Add(propertyForUi);
                 }
-            } else
-            {
-                foreach (var property in properties.ToList())
-                {
-                    if (property.PropertyAddress.Contains(request.SearchKey) || property.OwnerName.Contains(request.SearchKey))
-                    {
-                        var propertyForUi = _mapper.Map<Properties, PropertyForList>(property);
-                        propertyForUi.CampaignStatus = propertyForUi.CampaignStatus ?? "Not Assigned";
-                        propertyForList.Add(propertyForUi);
-                    }
-                }
+                w++;
             }
 
             return propertyForList;
