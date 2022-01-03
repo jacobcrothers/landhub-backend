8d9cd9349a993b8853cd239fbc2872b7e5905f58
jacobcrothers <jacobcrothers425@gmail.com>
2022-01-04 03:02:25 +0900

    Service for deligence.

---
+++ LandHubWebService/CommandHandler/CreateDueDeligenceCommandHandler.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+
+using Commands;
+
+using Domains.DBModels;
+
+using MediatR;
+
+using Services.Repository;
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CommandHandlers
+{
+    public class CreateDueDeligenceCommandHandler : AsyncRequestHandler<CreateDueDeligenceCommand>
+    {
+        private readonly IMapper _mapper;
+        private IBaseRepository<DueDeligence> _baseRepositoryDueDeligence;
+
+        public CreateDueDeligenceCommandHandler(IMapper mapper, IBaseRepository<DueDeligence> baseRepositoryDueDeligence)
+        {
+            _mapper = mapper;
+            _baseRepositoryDueDeligence = baseRepositoryDueDeligence;
+        }
+
+        protected override async Task Handle(CreateDueDeligenceCommand request, CancellationToken cancellationToken)
+        {
+            var dueDeligence = new DueDeligence()
+            {
+                OrgId = request.OrgId,
+                marketValue = request.marketValue,
+                propertyDimension = request.propertyDimension,
+                hoaRestriction = request.hoaRestriction,
+                zoingRestriction = request.zoingRestriction,
+                accessType = request.accessType,
+                visualAccess = request.visualAccess,
+                topography = request.topography,
+                powerAvailable = request.powerAvailable,
+                gasAvailable = request.gasAvailable,
+                pertTest = request.pertTest,
+                floodZone = request.floodZone,
+                survey = request.survey,
+                zoing = request.zoing,
+                utilities = request.utilities,
+                CreatedDate = request.CreatedDate
+            };
+            dueDeligence.Id = Guid.NewGuid().ToString();
+            await _baseRepositoryDueDeligence.Create(dueDeligence);
+        }
+    }
+}
---
+++ LandHubWebService/LandHubWebService/Controllers/DueDeligenceController.cs
@@ -0,0 +1,37 @@
+﻿
+using Commands;
+using Commands.Query;
+
+using Domains.DBModels;
+
+using MediatR;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Threading.Tasks;
+
+namespace PropertyHatchWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DueDeligenceController : BaseController
+    {
+        private readonly IMediator _mediator;
+        public DueDeligenceController(IMediator _mediator)
+        {
+            this._mediator = _mediator;
+        }
+        
+        [HttpPost("[action]")]
+        [Authorize]
+        public async Task<ActionResult> Add([FromBody] CreateDueDeligenceCommand createDueDeligenceCommand)
+        {
+            createDueDeligenceCommand.OrgId = SecurityContext.OrgId;
+            createDueDeligenceCommand.CreatedDate = DateTime.UtcNow;
+            await _mediator.Send(createDueDeligenceCommand);
+            return Ok();
+        }
+    }
+}
